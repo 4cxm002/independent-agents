89e40e1b8e8551d8fdc1ca6ec0c8b17edc44aa4c
Craig Moore <gdalemoore@gmail.com>
2016-03-11 12:59:13 -0500

    Checking in list.ts which will serve as a generic list

---
+++ custom/list.ts
@@ -0,0 +1,32 @@
+﻿module BlockTaming {
+    export class List<T>{
+        private items: Array<T>;
+
+        constructor() {
+            this.items = [];
+        }
+
+        size(): number {
+            return this.items.length;
+        }
+
+        add(value: T): void {
+            this.items.push(value);
+        }
+
+        get(index: number): T {
+            return this.items[index];
+        }
+
+        remove(value: T): void{
+            for (var i = 0; i < this.size(); i++)
+            {
+                var item = this.get(i);
+                if (item == value) {
+                    this.items.splice(i, 1);
+                    return;
+                }
+            }
+        }
+    }
+} 
